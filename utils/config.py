a098b810a60cdfecaa4fff7fd72a8d6f02abaa9c
walidaud <wdelileche@gmail.com>
2025-05-13 11:10:57 +0100

    a test run

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,18 @@
 # utils/config.py
 BASE_URL = "https://gorest.co.in/public/v2"
-ACCESS_TOKEN = "6aa7b70eff16c567746163e837641febc04bceb874345004da31743210c04be9"
+
+''' Access token should not be hardcoded. Use following code instead:
+import os
+ACCESS_TOKEN = os.getenv("GOREST_TOKEN")
+
+Go to CMD and set token with following line:
+set GOREST_TOKEN=your_token_here
+'''
+
+ACCESS_TOKEN = "6aa7b70eff16c567746163e837641febc04bceb874345004da31743210c04be9"   #should not be hardcoded
+
+
+
 HEADERS = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
     "Content-Type": "application/json"
